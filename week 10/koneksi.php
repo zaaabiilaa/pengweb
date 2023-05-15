831754ca995b5b06f0aea8bd8fee478d96ccfc97
Abrila Fatzali <108592061+zaaabiilaa@users.noreply.github.com>
2023-05-15 23:31:18 +0700

    Delete week 10 directory

--- week 10/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-// konfigurasi database
-$host       =   "localhost";
-$user       =   "root";
-$password   =   "";
-$database   =   "latihan";
-// perintah php untuk akses ke database
-$koneksi = mysqli_connect($host, $user, $password, $database);
-?>
